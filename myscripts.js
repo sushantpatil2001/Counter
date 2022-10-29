99d2dc3f51e85ef33c9a0029cfc28c8f782e3de2
Sushant Patil <psushant687@gmail.com>
2022-10-30 00:37:27 +0530

    first commit

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+var data=0;
+
+document.getElementById("counting").innerText=data;
+
+function increment(){
+    data = data+1;
+    document.getElementById("counting").innerText=data;
+}
+
+function decrement(){
+    data = data-1;
+    document.getElementById("counting").innerText=data;
+}
